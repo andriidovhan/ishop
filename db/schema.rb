959ce00d852faba7942ce7f50967bdb014f5bcfb
Andrii Dovhan <andrii.dovhan@onapp.com>
2016-10-24 10:17:16 +0300

    fix database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,6 @@ ActiveRecord::Schema.define(version: 20161018121356) do
     t.index ["user_id"], name: "index_accounts_on_user_id", using: :btree
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "carts", force: :cascade do |t|
     t.integer  "account_id"
     t.datetime "created_at", null: false
@@ -39,15 +32,6 @@ ActiveRecord::Schema.define(version: 20161018121356) do
   create_table "carts_products", id: false, force: :cascade do |t|
     t.integer "cart_id"
     t.integer "product_id"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -67,5 +51,4 @@ ActiveRecord::Schema.define(version: 20161018121356) do
   end
 
   add_foreign_key "accounts", "users"
-  add_foreign_key "comments", "articles"
 end
