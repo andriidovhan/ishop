1c12152e739287f9d6c467717f8f5d9a2e25702f
Andrii Dovhan <andrii.dovhan@onapp.com>
2016-10-26 20:01:03 +0300

    add  dependent: :destroy to user/account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,6 +1,6 @@
 class Account < ApplicationRecord
   belongs_to :user
-  has_one    :cart
+  has_one    :cart, dependent: :destroy
 
   validates :balance, numericality: { greater_than_or_equal_to: 0 }
 end
