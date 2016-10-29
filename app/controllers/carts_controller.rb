efa487043dec1886b126766ba1d111db343f90a2
Andrii Dovhan <andrii.dovhan@onapp.com>
2016-10-29 16:54:28 +0300

    fix cart show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,11 +1,16 @@
 class CartsController < ApplicationController
   def show
     @cart = Cart.find(session[:cart_id])
+    @products = @cart.products
   end
 
   def add
     @cart = Cart.find(session[:cart_id])
     @product = Product.find(params[:product_id])
     @cart.products << @product
+  end
+
+  def delete
+
   end
 end
