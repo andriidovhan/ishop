3882437269998efe3470b8042c46bdcf53eea718
Andrii Dovhan <andrii.dovhan@onapp.com>
2016-11-05 14:57:27 +0200

    bind localization to session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,12 @@ class ApplicationController < ActionController::Base
     session[:cart_id] ||= Cart.create.id
   end
 
+  # def set_locale
+  #   I18n.locale = params[:locale] || I18n.default_locale
+  # end
+
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+    session[:locale] = I18n.locale
   end
 end
