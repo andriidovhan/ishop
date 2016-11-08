d001f64b68ba6159a77b64b498530e525defbc6a
Andrii Dovhan <andrii.dovhan@onapp.com>
2016-11-08 11:23:47 +0200

    change port 587 -> 25 prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
-      port:                 587,
+      port:                 25,
       domain:               'gmail.com',
       user_name:            'dovhan.andriy@gmail.com',
       password:             '543*(iuyWQ',
